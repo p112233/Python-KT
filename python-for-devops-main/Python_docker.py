985d96f58843e44f97484fd177fd7a36922993eb
Prem Panda <148658293+p112233@users.noreply.github.com>
2023-12-22 01:16:34 +0530

    Update Python_docker.py

--- python-for-devops-main/Python_docker.py
+++ python-for-devops-main/Python_docker.py
@@ -1,1 +1,13 @@
+import subprocess
 
+image = input("input image name : ")
+container = input("input container  name :")
+ports  = input("input ports to be mapped : ")
+detach  = input("Do you wnat to run in deatch mode , enter y/n  : ")
+
+if detach == 'y':
+    subprocess.call("docker run --name %s -p %s -d %s"%(container,ports,image),shell=True)
+elif detach == 'n':
+    subprocess.call("docker run --name %s -p %s %s"%(container,ports,image),shell=True)
+else:
+    print("Invalid option")
